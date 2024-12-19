f49fa5763b17fadf4377e0b375b94d4b3a3bf878
elyakkos <124345737+elyakkos@users.noreply.github.com>
2024-12-19 10:30:37 +0300

    Update getweatherdata_test.py

--- lr3/open-weather_LW/getweatherdata_test.py
+++ lr3/open-weather_LW/getweatherdata_test.py
@@ -9,11 +9,11 @@ def test_without_key():
         get_weather_data("Moscow")
 
 def test_in_riga():
-    data = json.loads(get_weather_data("Riga", api_key=key))
+    data = json.loads(get_weather_data("Prague", api_key=key))
     assert data is not None
 
 def test_type_of_res():
-    data = get_weather_data("Riga", api_key=key)
+    data = get_weather_data("Prague", api_key=key)
     assert isinstance(data, str)
 
 def test_args_error():
@@ -25,7 +25,7 @@ def test_pos_arg_error():
         get_weather_data('', api_key=key)
 
 def test_coords_dim():
-    data = json.loads(get_weather_data('Riga', api_key=key))
+    data = json.loads(get_weather_data('Prague', api_key=key))
     assert len(data['coord']) == 2
 
 def test_temp_type():
@@ -33,9 +33,9 @@ def test_temp_type():
     assert isinstance(data['feels_like'], float)
 
 inp_params_1 = "city, api_key, expected_country"
-exp_params_countries = [("Chicago", key, 'US'),
+exp_params_countries = [("New York", key, 'US'),
                         ("Saint Petersburg", key, 'RU'), 
-                        ("Dakka", key, 'BD')]
+                        ("Severodvinsk", key, 'BD')]
 
 @pytest.mark.parametrize(inp_params_1, exp_params_countries)
 def test_countries(city, api_key, expected_country):
@@ -43,9 +43,9 @@ def test_countries(city, api_key, expected_country):
     assert data['country'] == expected_country
 
 inp_params_2 = "city, api_key, expected_time"
-exp_params_timezones = [("Chicago", key, 'UTC-5'),
+exp_params_timezones = [("New York", key, 'UTC-5'),
                         ("Saint Petersburg", key, 'UTC+3'),
-                        ("Dakka", key, 'UTC+6')]
+                        ("Severodvinsk", key, 'UTC+6')]
 
 @pytest.mark.parametrize(inp_params_2, exp_params_timezones)
 def test_utc_time(city, api_key, expected_time):
